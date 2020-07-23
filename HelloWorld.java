51086d73532511f0d74635abfc9f16c645fe2882
steventan1011 <tanbinkai1011@163.com>
2020-07-23 15:54:15 +0800

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[]args){
+        System.out.println("hello world");
+        System.out.println("1");
+        System.out.println("hello world");
+        System.out.println("2");
+    }
+}
