11faf7875823ebfea9a067bb1ab94d3f7ef4b075
steventan1011 <tanbinkai1011@163.com>
2020-08-05 18:36:23 +0800

    v 1.2.1

--- src/main/java/com/dcits/qtumforum/enums/NewsColumnEnum.java
+++ src/main/java/com/dcits/qtumforum/enums/NewsColumnEnum.java
@@ -1,13 +1,13 @@
 package com.dcits.qtumforum.enums;
 
 public enum NewsColumnEnum {
-    NEWS_COLUMN_HULIANWANG(1,"5572a109b3cdc86cf39001e3","量子计算"),
-    NEWS_COLUMN_KEJI(2,"5572a10ab3cdc86cf39001f4","量子通信"),
+    NEWS_COLUMN_HULIANWANG(1,"5572a109b3cdc86cf39001e3","量子通信"),
+    NEWS_COLUMN_KEJI(2,"5572a10ab3cdc86cf39001f4","量子计算"),
     NEWS_COLUMN_DIANNAO(3,"5572a10bb3cdc86cf39001f6","量子物理"),
     NEWS_COLUMN_SHUMA(4,"5572a10bb3cdc86cf39001f5","量子理论"),
     NEWS_COLUMN_KEPU(5,"572a10bb3cdc86cf39001f7","量子模拟"),
-    NEWS_COLUMN_TIYU(6,"5572a109b3cdc86cf39001e6","体育"),
-    NEWS_COLUMN_YULE(7,"5572a108b3cdc86cf39001d5","娱乐"),
+    NEWS_COLUMN_TIYU(6,"5572a109b3cdc86cf39001e6","量子测量"),
+    NEWS_COLUMN_YULE(7,"5572a108b3cdc86cf39001d5","密码学"),
     NEWS_COLUMN_GUONEI(8,"5572a108b3cdc86cf39001cd","国内"),
     ;
     private Integer code;
